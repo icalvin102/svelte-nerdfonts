17475ff07af70647dbf0b4e2661e20d3735bb821
icalvin102 <calvin@icalvin.de>
2020-09-11 01:55:31 +0200

    initial commit

---
+++ components/Icon.svelte
@@ -0,0 +1,28 @@
+<script>
+
+    export let data;
+    let className;
+
+    export { className as class };
+    
+</script>
+
+<svg aria-label="{data.iconname}"
+    viewBox="{data.viewbox}"
+    class="icon {className}">
+    {#each data.paths as path}
+        <path d="{path}"/>
+    {/each}
+</svg>
+
+
+<style>
+    svg {
+        transform: translateY(20%);
+        display: inline-block;
+        fill: currentColor;
+        height: 1em;
+        overflow: visible;
+    }
+
+</style>
